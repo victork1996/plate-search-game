6964f483e31d67df207be067c6400d262f575e96
Victor Kolesnikov <victork1996@gmail.com>
2020-06-22 22:42:52 +0300

    Feature/use csv lib (#1)
    * Now using the csv library to parse the records.

--- convert_csv_records_to_database.py
+++ convert_csv_records_to_database.py
@@ -1,8 +1,18 @@
 import sqlite3
 import argparse
+import csv
 
-_VALUE_SEPARATOR = '|'  # The file is not really COMMA separated values, but rather a '|' separated values.
-_VALUE_WRAPPER = '"'  # Each value is wrapped with this
+
+def register_csv_dialect():
+    """
+    Registers the custom dialect that's used in the CSV file.
+    :return: the name of the registered dialect.
+    """
+    _FIELD_DELIMITER = "|"
+    _DIALECT_NAME = "custom_dialect"
+
+    csv.register_dialect(_DIALECT_NAME, delimiter=_FIELD_DELIMITER, quoting=csv.QUOTE_ALL)
+    return _DIALECT_NAME
 
 
 def create_memory_db():
@@ -14,9 +24,9 @@ def create_memory_db():
                "`second` INTEGER, `third` INTEGER);")
 
     # Create indexes for super fast queries.
-    db.execute('CREATE INDEX first_part_index on records(first);')
-    db.execute('CREATE INDEX second_part_index on records(second);')
-    db.execute('CREATE INDEX third_part_index on records(third);')
+    db.execute("CREATE INDEX `first_part_index` on `records`(`first`);")
+    db.execute("CREATE INDEX `second_part_index` on `records`(`second`);")
+    db.execute("CREATE INDEX `third_part_index` on `records`(`third`);")
     return db
 
 
@@ -31,17 +41,20 @@ def split_plate_into_parts(plate_number):
         return int(plate_number[:3]), int(plate_number[3:-3]), int(plate_number[-3:])
 
 
-def insert_csv_line_into_db(line, db):
+def insert_csv_line_into_db(record, db):
     """
     Inserts a single CSV record into the DB. For now, we're interested only in the record's plate number.
-    """
-    values = line.split(_VALUE_SEPARATOR)
-    plate_number = values[0].strip(_VALUE_WRAPPER)
-    production_year = int(values[9].strip(_VALUE_WRAPPER))
 
-    # Plate numbers don't start with 0's. Any record that starts with a 0 is actually a 7 digit plate number.
+    :param record: the parsed fields of the CSV record.
+    :param db: the DB to insert the record into.
+    """
+    plate_number = record["mispar_rechev"]
+
+    # Plate numbers don't start with 0's. Any number that starts with a 0 is actually a 7 digit plate number.
     plate_number = plate_number.lstrip("0")
     first, second, third = split_plate_into_parts(plate_number)
+
+    production_year = int(record["shnat_yitzur"])
     db.execute("INSERT INTO `records` VALUES (?, ?, ?, ?, ?)", (production_year, plate_number, first, second, third))
 
 
@@ -60,18 +73,13 @@ def convert_csv_file_to_db(csv_file_path, output_db_path):
     """
     Converts a csv file of car records to an sqlite3 database.
     """
+    csv_dialect_name = register_csv_dialect()
+
     with create_memory_db() as db:
-        with open(csv_file_path, 'r') as csv_records_file:
-            # Skip the first line, it contains headers
-            csv_records_file.readline()
-
-            # Parse all the records
-            while True:
-                line = csv_records_file.readline()
-                if not line:
-                    break
-
-                insert_csv_line_into_db(line, db)
+        with open(csv_file_path) as csv_records_file:
+            records_reader = csv.DictReader(csv_records_file, dialect=csv_dialect_name)
+            for record in records_reader:
+                insert_csv_line_into_db(record, db)
 
         db.commit()
         write_output_db_file(db, output_db_path)
@@ -85,5 +93,5 @@ def main():
     convert_csv_file_to_db(args.csv_file_path, args.output_db_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- rarity_checker.py
+++ rarity_checker.py
@@ -12,7 +12,7 @@ def get_latest_production_year_and_appearance_count(db, number):
     Returns the latest production year of a car with the specified number in its plate and the number's
      appearance count.
     """
-    return db.execute("SELECT MAX(production_year), COUNT(*) from `records` WHERE `first`=:1 OR "
+    return db.execute("SELECT MAX(`production_year`), COUNT(*) from `records` WHERE `first`=:1 OR "
                       "second=:1 OR `third`=:1;", (number,)).fetchone()
 
 
@@ -27,9 +27,9 @@ def check_number_rarity(records_db_path, number_to_check):
         total_count = get_total_record_count(db)
         latest_production_year, appearance_count = get_latest_production_year_and_appearance_count(db, number_to_check)
 
-    print("The number {} appears on {} plates. ".format(number_to_check, appearance_count))
-    print("The chance to find it is 1 in {}.".format(total_count / appearance_count))
-    print("The latest production year of a car with that number is {}.".format(latest_production_year))
+    print(f"The number {number_to_check} appears on {appearance_count} plates out of a total of {total_count:,}.")
+    print(f"The chance to find it is 1 in {total_count/appearance_count}.")
+    print(f"The latest production year of a car with that number is {latest_production_year}.")
 
 
 def main():
@@ -41,5 +41,5 @@ def main():
     check_number_rarity(args.records_db_path, args.number_to_check)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
