d6aa8125dfaf39670c2c7eec3b739381ef5fde8f
Victor Kolesnikov <victork1996@gmail.com>
2020-06-27 20:35:04 +0300

    Made some changes in the main function.

--- plates_game/__init__.py
+++ plates_game/__init__.py
@@ -1,2 +1,2 @@
-from plates_game.rarity_checker import check_number_rarity
+from plates_game.rarity_checker import get_total_record_count, check_number_rarity
 from plates_game.convert_csv_records_to_database import convert_csv_file_to_db
--- plates_game/rarity_checker.py
+++ plates_game/rarity_checker.py
@@ -2,8 +2,9 @@ import sqlite3
 import argparse
 
 
-def get_total_record_count(db):
-    (total_count,) = db.execute("SELECT COUNT (`plate_number`) FROM `records`;").fetchone()
+def get_total_record_count(records_db_path):
+    with sqlite3.connect(records_db_path) as db:
+        (total_count,) = db.execute("SELECT COUNT (`plate_number`) FROM `records`;").fetchone()
     return total_count
 
 
@@ -23,23 +24,27 @@ def check_number_rarity(records_db_path, number_to_check):
     :param records_db_path: a path to a DB containing records of israeli plates.
     :param number_to_check: the number to check.
     """
+    if number_to_check < 0 or number_to_check > 999:
+        raise ValueError("Number should be between 0 and 999")
+
     with sqlite3.connect(records_db_path) as db:
-        total_count = get_total_record_count(db)
         latest_production_year, appearance_count = get_latest_production_year_and_appearance_count(db, number_to_check)
 
-    print(f"The number {number_to_check} appears on {appearance_count} plates out of a total of {total_count:,}.")
-    print(f"The chance to find it is 1 in {total_count/appearance_count}.")
+    return appearance_count, latest_production_year
+
+
+def main(args):
+    total_count = get_total_record_count(args.records_db_path)
+    appearance_count, latest_production_year = check_number_rarity(args.records_db_path, int(args.number_to_check))
+    print(f"The number {args.number_to_check} appears on {appearance_count:,}"
+          f" plates out of a total of {total_count:,}.")
+
+    print(f"The chance to find it is 1 in {int(total_count / appearance_count)}.")
     print(f"The latest production year of a car with that number is {latest_production_year}.")
 
 
-def main():
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("records_db_path")
     parser.add_argument("number_to_check")
-    args = parser.parse_args()
-
-    check_number_rarity(args.records_db_path, args.number_to_check)
-
-
-if __name__ == "__main__":
-    main()
+    main(parser.parse_args())
