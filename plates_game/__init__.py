f6140786016282868bff3b28d079da9b2fa8d26f
Victor Kolesnikov <victork1996@gmail.com>
2020-06-27 19:47:34 +0300

    Now it's a package :O

---
+++ plates_game/__init__.py
@@ -0,0 +1,2 @@
+from plates_game.rarity_checker import check_number_rarity
+from plates_game.convert_csv_records_to_database import convert_csv_file_to_db
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='plates-game',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Victor',
+    author_email='victork1996@gmail.com',
+    description=''
+)
