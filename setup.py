873c687e544176d7da9f5c40c7c42f2ea9f83112
Victor Kolesnikov <victork1996@gmail.com>
2022-02-21 14:31:53 +0200

    Added plates_game as a package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='plates-game',
     version='1.0.0',
-    packages=[''],
+    packages=['plates_game'],
     url='',
     license='',
     author='Victor',
